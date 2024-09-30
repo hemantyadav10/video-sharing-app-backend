7bc895b206e36b226ce5252ae81620102104f9fc
Hemant <yad.heman@gmail.com>
2024-09-30 21:13:35 +0530

    Add MongoDB connection logic

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import { DB_NAME } from "../constants.js";
+
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log('/n MongoDb connected!! Db Host: ', connectionInstance.connection.host);
+
+  } catch (error) {
+    console.log('MongoDb connection error: ', error);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
