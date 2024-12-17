9f5c63297bc4912509ce6696595f18d061bcf1fa
Hemant <yad.heman@gmail.com>
2024-12-17 19:09:10 +0530

    Remove password and refresh token from final return response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -89,55 +89,58 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 // Handle user login
-  const loginUser = asyncHandler(async (req, res) => {
-    const { username, email, password } = req.body;
+const loginUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
 
-    if (!username && !email) {
-      throw new ApiError(400, "Either username or email is required");
-    }
+  if (!username && !email) {
+    throw new ApiError(400, "Either username or email is required");
+  }
 
-    if (!password) {
-      throw new ApiError(400, "password is required");
-    }
+  if (!password) {
+    throw new ApiError(400, "password is required");
+  }
 
-    const user = await User.findOne({
-      $or: [{ username }, { email }]
-    })
-
-    if (!user) {
-      throw new ApiError(404, "User not found with the provided username or email")
-    }
-
-    const isPasswordValid = await user.isPasswordCorrect(password);
-
-    if (!isPasswordValid) {
-      throw new ApiError(401, 'Invalid credentials')
-    }
-
-    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user);
-
-    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
-
-    const options = {
-      httpOnly: true,
-      secure: true,
-    }
-
-    return res
-      .status(200)
-      .cookie("accessToken", accessToken, options)
-      .cookie("refreshToken", refreshToken, options)
-      .json(new ApiResponse(
-        200,
-        {
-          user: loggedInUser,
-          accessToken, 
-          refreshToken
-        },
-        "User logged in successfully"
-      ))
-
+  const user = await User.findOne({
+    $or: [{ username }, { email }]
   })
+
+  if (!user) {
+    throw new ApiError(404, "User not found with the provided username or email")
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, 'Invalid credentials')
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user);
+
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  }
+
+  const userResponse = user.toObject();
+  delete userResponse.password;
+  delete userResponse.refreshToken;
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(new ApiResponse(
+      200,
+      {
+        user: userResponse,
+        accessToken,
+        refreshToken
+      },
+      "User logged in successfully"
+    ))
+
+})
 
 // Handle user logout
 const logoutUser = asyncHandler(async (req, res) => {
@@ -276,6 +279,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
 // Controller for updating avatar
 const updateUserAvatar = asyncHandler(async (req, res) => {
   const avatarLocalPath = req.file?.path;
+  console.log(avatarLocalPath)
 
   if (!avatarLocalPath) {
     throw new ApiError(400, 'Avatar file is missing')
